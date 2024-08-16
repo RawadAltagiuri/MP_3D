6809f9ba6cd94a566a94bf78246830720438defc
evolabpc <evolab@khas.edu.tr>
2024-08-16 15:54:29 +0300

    Added docs to grow and retract

--- grow.m
+++ grow.m
@@ -1,3 +1,15 @@
+%{
+Takes sp, configuration and amount of growth and returns the resulting
+configuration from growing config to that amount.
+
+Inputs:
+- sp, the problem specification
+- conf, config to grow from
+- amountOfGrow, amount to grow
+
+Outputs:
+- config, the resulting config from growth of the previous one
+%}
 function conf = grow(sp, conf, amountOfGrow)
     for lastExpanded = size(conf, 1):-1:1
         if conf(lastExpanded, 3) ~= 0
--- retract.m
+++ retract.m
@@ -1,3 +1,18 @@
+%{
+Takes sp, configuration and amount of retraction and returns the resulting
+configuration from retraction config that amount.
+Important thing to notice here is that this function does NOT take any
+robot constraints into account. That is, retraction is simply the collapse
+of the last link.
+
+Inputs:
+- conf, config to retract from
+- amountOfRet, amount to retraction
+
+Outputs:
+- config, the resulting config from retraction of the previous one
+%}
+
 function conf = retract(conf, amountOfRet)
     for lastExpanded = size(conf, 1):-1:1
         if conf(lastExpanded, 3) ~= 0
