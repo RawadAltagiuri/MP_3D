4ec1d5ec76b14e485ef7dbfd689ae827a2387de7
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-11-14 13:07:07 +0300

    Ozan's collision detector

--- collisionCheck.m
+++ collisionCheck.m
@@ -10,7 +10,7 @@ function intersects = collisionCheck(conf,op)
     intersects = false;
     for i = 1 : nUsedNodes - 1 
         for j = 1 : nObstacles
-            if segmentxcylinder(nodes(i,:),nodes(i+1,:),obstacles(j,:))
+            if veccol(nodes(i,:),nodes(i+1,:),obstacles(j,:))
                 intersects = true;
                 return;
             end
